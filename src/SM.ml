045b281882c446a940fe33e946daa732f1231d25
fresheed <ok.john.rus@gmail.com>
2019-03-20 15:32:34 +0300

    copied previous code

--- src/SM.ml
+++ src/SM.ml
@@ -12,7 +12,6 @@ open Language
 (* a label                         *) | LABEL of string
 (* unconditional jump              *) | JMP   of string                                                                                                                
 (* conditional jump                *) | CJMP  of string * string with show
-                                                   
 (* The type for the stack machine program *)                                                               
 type prg = insn list
 
@@ -28,9 +27,20 @@ type config = int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let rec eval env conf prog = failwith "Not yet implemented"
-
-(* Top-level evaluation
+(* let rec eval env conf prog = failwith "Not yet implemented" *)
+let rec eval env config2 program =
+  let eval_cmd config command = match config, command with
+    | (y::x::stack, ((state, inp, out) as subconf)), BINOP (str_op)
+      -> let result=Language.Expr.eval state (Language.Expr.Binop (str_op, Language.Expr.Const (x), Language.Expr.Const (y)))
+         in (result::stack, subconf)
+    | (stack, subconf), CONST (value) -> (value::stack, subconf)
+    | (stack, (state, value::inp, out)), READ -> (value::stack, (state, inp, out))
+    | (value::stack, (state, inp, out)), WRITE -> (stack, (state, inp, out@[value]))
+    | (stack, ((state, _, _) as subconf)), LD (var) -> ((state var)::stack, subconf)
+    | (value::stack, (state, inp, out)), ST (var) -> (stack, (Language.Expr.update var value state, inp, out))
+  in match config2, program with
+     | config2, [] -> config2
+     | config2, cmd::rest -> eval env (eval_cmd config2 cmd) rest(* Top-level evaluation
 
      val run : prg -> int list -> int list
 
@@ -53,4 +63,14 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+let rec compile =
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in
+  function
+  | Stmt.Seq (s1, s2)  -> compile s1 @ compile s2
+  | Stmt.Read x        -> [READ; ST x]
+  | Stmt.Write e       -> expr e @ [WRITE]
+  | Stmt.Assign (x, e) -> expr e @ [ST x]
