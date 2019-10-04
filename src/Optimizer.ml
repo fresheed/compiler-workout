73158b7fa53dd3d25366b3bea7d5ee9d59248855
fresheed <ok.john.rus@gmail.com>
2019-10-04 12:58:50 +0300

    refactoring

--- src/Optimizer.ml
+++ src/Optimizer.ml
@@ -2,10 +2,28 @@ open GT
 open Language
 open Set
 
+module Int' = struct
+  type t = int
+  let compare x y = Pervasives.compare x y
+end
+module SetInt = Set.Make(Int')
+module Int2' = struct
+  type t = int * int
+  let compare (x,a) (y, b) = let one = Pervasives.compare x y in if (one != 0) then one else Pervasives.compare a b
+end
+module SetInt2 = Set.Make(Int2')
+module Expr' = struct
+  type t = Expr.t
+  let compare x y = if (Pervasives.compare x y != 0) then 1 else 0
+end
+module SetExpr = Set.Make(Expr')
 
-module ExtStmt =
-  struct
+let rec show_expr expr = match expr with
+| Expr.Const i -> Printf.sprintf "%d" i
+| Expr.Var x -> x
+| Expr.Binop (op, e1, e2) -> Printf.sprintf "(%s)%s(%s)" (show_expr e1) op (show_expr e2)
 
+module ExtStmt =  struct
     @type t =
       (* read into the variable           *) | ExtRead   of int * string
       (* write the value of an expression *) | ExtWrite  of int * Expr.t
@@ -15,26 +33,6 @@ module ExtStmt =
       (* conditional                      *) | ExtIf     of int * Expr.t * t * t
       (* loop with a pre-condition        *) | ExtWhile  of int * Expr.t * t
       (* loop with a post-condition       *) | ExtRepeatUntil of int * Expr.t * t with show
-
-    module Int' = struct
-      type t = int
-      let compare x y = Pervasives.compare x y
-    end
-    module SetInt = Set.Make(Int')
-    module Int2' = struct
-      type t = int * int
-      let compare (x,a) (y, b) = let one = Pervasives.compare x y in if (one != 0) then one else Pervasives.compare a b
-    end
-    module SetInt2 = Set.Make(Int2')
-    module Expr' = struct
-      type t = Expr.t
-      let compare x y = if (Pervasives.compare x y != 0) then 1 else 0
-    end
-    module SetExpr = Set.Make(Expr')
-
-(*    module ESP' = struct*)
-(*      type t = t*)
-(*      let compare = compare*)
 
     let get_label estmt = match estmt with
       | ExtRead (i, _) | ExtWrite (i, _) | ExtAssign (i, _, _) | ExtSeq (i, _, _)
@@ -56,11 +54,6 @@ module ExtStmt =
         | Stmt.Skip -> ExtSkip (num)
       in enhance_ prog 0
 
-    let rec show_expr expr = match expr with
-    | Expr.Const i -> Printf.sprintf "%d" i
-    | Expr.Var x -> x
-    | Expr.Binop (op, e1, e2) -> Printf.sprintf "(%s)%s(%s)" (show_expr e1) op (show_expr e2)
-
     let tostring eprog lbl_proc =
       let rec show_ stmt shift =
         let next = String.concat "" [" "; shift] in
@@ -77,11 +70,14 @@ module ExtStmt =
 
         let print_ext stmt  = (match stmt with
           | ExtSeq _ as es -> describe es
-          (*| ExtSeq _ as es -> Printf.sprintf "%d: %s" (get_label es) (describe es)*)
           | other -> Printf.sprintf "%d: %s #%s" (get_label stmt) (describe stmt) (lbl_proc (get_label stmt)))
       in  print_ext stmt
     in show_ eprog ""
 
+end
+
+module AnalysisGeneral = struct
+    open ExtStmt
     let rec get_init_label estmt = match estmt with
       | ExtSeq (i, s1, s2) -> get_init_label s1
       | ExtIf (i, cond, s1, s2) -> i
@@ -122,33 +118,61 @@ module ExtStmt =
       | Expr.Binop (_, e1, e2) as cur -> let subs = SetExpr.union (get_all_subexprs e1) (get_all_subexprs e2)
                                          in SetExpr.add cur subs
       | _ -> SetExpr.empty
+      
 
+    let prog_exprs prog =
+      let rec stmt_exrs stmt = match stmt with
+      | ExtSeq (i, s1, s2) -> SetExpr.union (stmt_exrs s1) (stmt_exrs s2)
+      | ExtIf (i, cond, s1, s2) -> SetExpr.union (get_all_subexprs cond) (SetExpr.union (stmt_exrs s1) (stmt_exrs s2))
+      | ExtWhile (i, cond, s) -> SetExpr.union (get_all_subexprs cond) (stmt_exrs s)
+      | ExtRepeatUntil (i, cond, s) -> SetExpr.union (get_all_subexprs cond) (stmt_exrs s)
+      | ExtRead (i, x) -> SetExpr.empty
+      | ExtWrite (_, e) -> get_all_subexprs e
+      | ExtAssign (_, x, e) -> get_all_subexprs e
+      | ExtSkip _ -> SetExpr.empty
+      in stmt_exrs prog
+
+    let map_build_set emp jn mapper args_list = let results = List.map mapper args_list in
+                                    (match results with
+                                    | [] -> emp
+                                    | h :: rest -> List.fold_left (fun acc set -> jn acc set) h rest)
+
+
+    let get_by_label prog label =
+      let is_none opt = match opt with | Some x -> false | None -> true in
+      let force_get opt = match opt with | Some x -> x | None -> failwith "Optional is None" in
+      let rec find_by_label label stmt =
+        if (get_label stmt == label) then Some stmt
+        else (match stmt with
+        | ExtSeq (_, s1, s2) -> let res1 = find_by_label label s1 in
+                                if (is_none res1) then find_by_label label s2 else res1
+        | ExtIf (_, _, s1, s2) -> let res1 = find_by_label label s1 in
+                                  if (is_none res1) then find_by_label label s2 else res1
+        | ExtWhile (_, _, s) -> find_by_label label s
+        | ExtRepeatUntil (_, cond, s) -> find_by_label label s
+        | _ -> None) in
+      force_get (find_by_label label prog)
+
+    let get_successors prog label = let edges = SetInt2.filter (fun (l_from, _) -> l_from == label) (get_flow prog) in
+                                     SetInt.of_list (List.map snd (SetInt2.elements edges))
+end
+
+module VB = struct
+    open AnalysisGeneral
+    open ExtStmt
     let analyze_vb prog =
-      let prog_finish_labels = get_finish_labels prog in
-      let prog_exprs =
-        let rec stmt_exrs stmt = match stmt with
-        | ExtSeq (i, s1, s2) -> SetExpr.union (stmt_exrs s1) (stmt_exrs s2)
-        | ExtIf (i, cond, s1, s2) -> SetExpr.union (get_all_subexprs cond) (SetExpr.union (stmt_exrs s1) (stmt_exrs s2))
-        | ExtWhile (i, cond, s) -> SetExpr.union (get_all_subexprs cond) (stmt_exrs s)
-        | ExtRepeatUntil (i, cond, s) -> SetExpr.union (get_all_subexprs cond) (stmt_exrs s)
-        | ExtRead (i, x) -> SetExpr.empty
-        | ExtWrite (_, e) -> get_all_subexprs e
-        | ExtAssign (_, x, e) -> get_all_subexprs e
-        | ExtSkip _ -> SetExpr.empty
-        in stmt_exrs prog in
-      let map_build_set emp jn mapper args_list = let results = List.map mapper args_list in
-                                      (match results with
-                                      | [] -> emp
-                                      | h :: rest -> List.fold_left (fun acc set -> jn acc set) h rest) in
       (* list of killed (sub)expressions *)
+      let prog_finish_labels = get_finish_labels prog in
+      let is_finish stmt = (SetInt.mem (get_label stmt) prog_finish_labels) in
+      let depend_on x = map_build_set (SetExpr.empty) (SetExpr.union) (get_dependent_subexprs x) (SetExpr.elements (prog_exprs prog)) in
       let kills stmt = match stmt with
         | ExtSeq _ -> failwith ("Should not be called on seq statements")
         | ExtIf _ -> SetExpr.empty
         | ExtWhile _ -> SetExpr.empty
         | ExtRepeatUntil _ -> SetExpr.empty
-        | ExtRead (_, x) -> map_build_set (SetExpr.empty) (SetExpr.union) (get_dependent_subexprs x) (SetExpr.elements prog_exprs)
+        | ExtRead (_, x) -> depend_on x
         | ExtWrite _ -> SetExpr.empty
-        | ExtAssign (_, x, _) -> map_build_set (SetExpr.empty) (SetExpr.union) (get_dependent_subexprs x) (SetExpr.elements prog_exprs)
+        | ExtAssign (_, x, _) -> depend_on x
         | ExtSkip _ -> SetExpr.empty in
       (* list of generated (sub)expressions *)
       let gens stmt = match stmt with
@@ -160,36 +184,20 @@ module ExtStmt =
         | ExtWrite (_, e) -> get_all_subexprs e
         | ExtAssign (_, _, e) -> get_all_subexprs e
         | ExtSkip _ -> SetExpr.empty in
-      let is_finish stmt = (SetInt.mem (get_label stmt) prog_finish_labels) in
-      let get_by_label label =
-        let is_none opt = match opt with | Some x -> false | None -> true in
-        let force_get opt = match opt with | Some x -> x | None -> failwith "Optional is None" in
-        let rec find_by_label label stmt =
-          if (get_label stmt == label) then Some stmt
-          else (match stmt with
-          | ExtSeq (_, s1, s2) -> let res1 = find_by_label label s1 in
-                                  if (is_none res1) then find_by_label label s2 else res1
-          | ExtIf (_, _, s1, s2) -> let res1 = find_by_label label s1 in
-                                    if (is_none res1) then find_by_label label s2 else res1
-          | ExtWhile (_, _, s) -> find_by_label label s
-          | ExtRepeatUntil (_, cond, s) -> find_by_label label s
-          | _ -> None) in
-        force_get (find_by_label label prog) in
-      let get_successors label = let edges = SetInt2.filter (fun (l_from, _) -> l_from == label) (get_flow prog) in
-                                 SetInt.of_list (List.map snd (SetInt2.elements edges)) in
-      let rec vb_entry label = let stmt = get_by_label label
+      let by_label = get_by_label prog in
+      let rec vb_entry label = let stmt = by_label label
                                in SetExpr.union (SetExpr.diff (vb_exit label) (kills stmt)) (gens stmt)
-          and vb_exit label = let stmt = get_by_label label
+          and vb_exit label = let stmt = by_label label
                               in if (is_finish stmt) then SetExpr.empty
-                                 else let preds_labels = get_successors label in
+                                 else let preds_labels = get_successors prog label in
                                  map_build_set (SetExpr.empty) (SetExpr.inter) vb_entry (SetInt.elements preds_labels) in
       (vb_entry, vb_exit)
-
 end
 
+
 let describe_vb prog label =
-  let on_entry, on_exit = ExtStmt.analyze_vb prog in
-  let show_list set = (String.concat ", " (List.map ExtStmt.show_expr (ExtStmt.SetExpr.elements set))) in
+  let on_entry, on_exit = VB.analyze_vb prog in
+  let show_list set = (String.concat ", " (List.map show_expr (SetExpr.elements set))) in
   Printf.sprintf "On entry: %s # On exit: %s"
                  (show_list (on_entry label)) (show_list (on_exit label))
 
